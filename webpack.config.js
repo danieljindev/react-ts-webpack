d226e6bd57492c0e719f839ffd2536bda4ae894b
Daniel Jin <danieljin123@outlook.com>
2021-06-12 07:11:46 +0300

    Initialize react with typescript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: '[name].[fullhash].bundle.js',
+    publicPath: '/',
+  },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "src")
+  },
+  module: {
+    rules: [
+      /**
+       * TypeScript (.ts/.tsx files)
+       *
+       * The TypeScript loader will compile all .ts/.tsx files to .js. Babel is
+       * not necessary here since TypeScript is taking care of all transpiling.
+       */
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      // Styles
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1 },
+          },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+      // Fonts
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/inline',
+      },
+      // Markdown
+      {
+        test: /\.md$/,
+        type: 'asset/source',
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
